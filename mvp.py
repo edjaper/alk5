a143b03617d322f8a748d4eb9c907ecc8c0f35fb
edjaper <66147047+edjaper@users.noreply.github.com>
2023-01-14 03:35:58 -0300

    Update mvp.py

--- mvp.py
+++ mvp.py
@@ -49,12 +49,12 @@ if st.button('Mostrar Explicacao'):
    
 
 page_names_to_funcs = {
-    "—": intro,
-    "Plotting Demo": plotting_demo,
-    "Mapping Demo": mapping_demo,
-    "DataFrame Demo": data_frame_demo
+    '—': intro,
+    'Plotting Demo': plotting_demo,
+    'Mapping Demo': mapping_demo,
+    'DataFrame Demo': data_frame_demo
 }
 
-demo_name = st.sidebar.selectbox("Visualizar", page_names_to_funcs.keys())
+demo_name = st.sidebar.selectbox('Visualizar', page_names_to_funcs.keys())
 page_names_to_funcs[demo_name]()    
     
