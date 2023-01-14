78e4e46765c55e624bf78769a457fe28e5d9b161
edjaper <66147047+edjaper@users.noreply.github.com>
2023-01-14 04:37:30 -0300

    Update mvp.py

--- mvp.py
+++ mvp.py
@@ -61,7 +61,40 @@ def intro():
     """
     )
     
+def plotting():
+    import streamlit as st
+    import time
+    import numpy as np
 
+    st.markdown(f'# {list(page_names_to_funcs.keys())[1]}')
+    st.write(
+        """
+        O gráfico mostra o pIC50 real versus o predito pelo modelo. Você pode anotar os identificadores de moléculas que deseja inspecionar no menu **Ver explicação**
+"""
+    )
+
+    progress_bar = st.sidebar.progress(0)
+    status_text = st.sidebar.empty()
+    last_rows = np.random.randn(1, 1)
+    chart = st.line_chart(last_rows)
+
+    for i in range(1, 101):
+        new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
+        status_text.text("%i%% Complete" % i)
+        chart.add_rows(new_rows)
+        progress_bar.progress(i)
+        last_rows = new_rows
+        time.sleep(0.05)
+
+    progress_bar.empty()
+
+    # Streamlit widgets automatically run the script from top to bottom. Since
+    # this button is not connected to any other logic, it just causes a plain
+    # rerun.
+    st.button("Re-run")
+
+    
+    
 def lime():
     import streamlit as st
 
@@ -77,7 +110,7 @@ def lime():
     
 page_names_to_funcs = {
     "Sobre": intro,
-    #'Plotting Demo': plotting_demo,
+    #"Visão Geral": plotting,
     #'Mapping Demo': mapping_demo,
     "Ver explicação": lime
 }
