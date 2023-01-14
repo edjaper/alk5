6891856fe1e008fc66f5750d3b31913ddc75da2b
edjaper <66147047+edjaper@users.noreply.github.com>
2023-01-14 03:42:23 -0300

    Update mvp.py

--- mvp.py
+++ mvp.py
@@ -36,11 +36,40 @@ explainer1 = lime.lime_tabular.LimeTabularExplainer(np.array(df_x),feature_names
 #Caching the model for faster loading
 #@st.cache
 
+def intro():
+    import streamlit as st
 
+    st.write("# Welcome to Streamlit! 👋")
+    st.sidebar.success("Select a demo above.")
+
+    st.markdown(
+        """
+        Streamlit is an open-source app framework built specifically for
+        Machine Learning and Data Science projects.
+
+        **👈 Select a demo from the dropdown on the left** to see some examples
+        of what Streamlit can do!
+
+        ### Want to learn more?
+
+        - Check out [streamlit.io](https://streamlit.io)
+        - Jump into our [documentation](https://docs.streamlit.io)
+        - Ask a question in our [community
+          forums](https://discuss.streamlit.io)
+
+        ### See more complex demos
+
+        - Use a neural net to [analyze the Udacity Self-driving Car Image
+          Dataset](https://github.com/streamlit/demo-self-driving)
+        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+    """
+    )
+
+    
 page_names_to_funcs = {
     '—': intro,
     'Plotting Demo': plotting_demo,
-    'Mapping Demo': mapping_demo,
+    #'Mapping Demo': mapping_demo,
     'DataFrame Demo': data_frame_demo
 }
 
