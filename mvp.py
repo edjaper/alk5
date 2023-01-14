0b607bcdd6d0e55a14a4a756a952adadcd93b63a
edjaper <66147047+edjaper@users.noreply.github.com>
2023-01-14 03:34:45 -0300

    Update mvp.py

--- mvp.py
+++ mvp.py
@@ -45,3 +45,16 @@ i = st.number_input('ID Instancia:', min_value=0, max_value=544, value=0)
 if st.button('Mostrar Explicacao'):
     explanation = explainer1.explain_instance(df_x.iloc[i,:], estimator.predict, num_features=10)
     st.pyplot(explanation.as_pyplot_figure())
+
+   
+
+page_names_to_funcs = {
+    "—": intro,
+    "Plotting Demo": plotting_demo,
+    "Mapping Demo": mapping_demo,
+    "DataFrame Demo": data_frame_demo
+}
+
+demo_name = st.sidebar.selectbox("Visualizar", page_names_to_funcs.keys())
+page_names_to_funcs[demo_name]()    
+    
